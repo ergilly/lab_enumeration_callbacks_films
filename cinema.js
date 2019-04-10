885f543c30dbbf892e484228d52fd93bb71633d1
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-04-10 15:01:36 +0100

    All tests passed

--- cinema.js
+++ cinema.js
@@ -1,9 +1,64 @@
-class Cinema{
+class Cinema {
 
   constructor(films) {
     this.films = films;
   };
-  
-}
 
+
+
+
+
+  listTitles() {
+    return this.films.map(film => film.title);
+  };
+
+  findFilmByTitle(title) {
+    return this.films.filter(function(film) {
+      return film.title.includes(title);
+    });
+  };
+
+  findFilmsByGenre(genre) {
+    return this.films.filter(function(film) {
+      return film.genre.includes(genre);
+    });
+  };
+
+
+  findFilmsByYear(year) {
+    let result = this.films.filter(function(film) {
+      return film.year === year;
+    });
+    if (result.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  overMaxLength(length) {
+    let result = this.films.filter(function(film) {
+      return film.length >= length;
+    });
+    if (result.length > 0) {
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+  totalRuntime() {
+    return this.films.reduce(function(total, time) {
+      return total + time.length;
+    }, 0);
+  };
+
+  findFilmByProperty(property, value) {
+    return this.films.filter(function(film) {
+
+      return film[property] === value;
+    });
+  }
+
+};
 module.exports = Cinema;
--- film.js
+++ film.js
@@ -7,4 +7,6 @@ class Film{
   };
 }
 
+
+
 module.exports = Film;
